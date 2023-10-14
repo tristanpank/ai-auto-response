3482134f9e73fb399c76ece6318261f634b211bd
DiegoCarra <forgamesonly057@gmail.com>
2023-10-14 13:06:14 -0400

    Create phone

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Input from './components/Input';
+import Phone from './components/Phone';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-screen">
       <Input />
+      <div className='h-3/5'>
+        <Phone />
+      </div>
+      
     </div>
   );
 }
