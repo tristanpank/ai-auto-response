9bf612f69a8f0bb302bddd16bdcfdb55fb607e17
Tristan Pank <panktristan@gmail.com>
2023-10-15 09:32:57 -0400

    A little CSS

--- frontend/src/components/ContextForm.jsx
+++ frontend/src/components/ContextForm.jsx
@@ -11,11 +11,15 @@ export default function ContextForm({context, setContext}) {
   }
 
   return (
-    <form className="flex justify-center items-center" onSubmit={handleSubmit}>
-      <label className="text-white">Why are you busy?</label>
-      <input type="text" onChange={(e) => setReason(e.target.value)} placeholder="At a Doctor's Appointment"></input>
-      <label className="text-white">How long are you busy?</label>
-      <input type="text" onChange={(e) => setDuration(e.target.value)} placeholder="2 hours"></input>
+    <form className="flex flex-col justify-center items-center gap-3 mt-5" onSubmit={handleSubmit}>
+      <div className="flex gap-5">
+        <label className="text-white">Why are you busy?</label>
+        <input type="text" onChange={(e) => setReason(e.target.value)} placeholder="At a Doctor's Appointment"></input>
+      </div>
+      <div className="flex gap-5">
+        <label className="text-white">How long are you busy?</label>
+        <input type="text" onChange={(e) => setDuration(e.target.value)} placeholder="2 hours"></input>
+      </div>
       <button type="submit" onSubmit={handleSubmit} className="text-white">Change Context</button>
     </form>
   )
